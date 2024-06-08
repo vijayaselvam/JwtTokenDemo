9243d608ee4985b87a7dd8dbb21ff143887204fd
vijayaselvam <vijayaselvam@users.noreply.github.com>
2024-06-08 10:39:23 +0530

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using JwtTokenDemo.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtTokenDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IConfiguration Configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Auth([FromBody] User user)
+        {
+            IActionResult response = Unauthorized();
+
+            if (User != null)
+            {
+
+                if (user.UserName.Equals("jwttest@email.com") && user.Password.Equals("test@000"))
+                {
+                    var issuer = Configuration["Jwt:Issuer"];
+                    var audience = Configuration["Jwt:Audience"];
+                    var key = Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]);
+                    var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature);
+
+                    var subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Email, user.UserName),
+                    });
+
+                    var expires = DateTime.UtcNow.AddMinutes(10);
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = subject,
+                        Expires = expires,
+                        Issuer = issuer,
+                        Audience = audience,
+                        SigningCredentials = signingCredentials
+                    };
+
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    var jwtToken = tokenHandler.WriteToken(token);
+
+                    return Ok(jwtToken);
+
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ DTO/User.cs
@@ -0,0 +1,13 @@
+﻿namespace JwtTokenDemo.DTO
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+    }
+}
